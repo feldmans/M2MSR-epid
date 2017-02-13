21f58e37cc8c6a02f51e35990a7304a0bb679e8a
Sarah F <sarah.f.feldman@gmail.com>
2017-02-13 19:43:39 +0100

    forest plot : légende en ordonnée, IC mieux espacés

--- src/epid_data_management.R
+++ src/epid_data_management.R
@@ -505,35 +505,42 @@ getMIC <- function(.data){
 
 df <- getMIC(dta_m)
 df <- na.omit(df)
+head(df)
 
+m<-tapply(df$myy, df$var, mean)
+m<-data.frame(var=names(m), y=m)
+head(m)
+
+df2<-merge(df, m, by="var", all=T)
+df2<-df2[order(df2$var, df2$group),]
+df2$y<-ifelse(df2$groupe=="SWAN", 0.25, -0.25)+df2$y
+head(df2)
+
+
+
+laby<-unique(df2$var)
 
 col <- hue_pal()(length(1:2))
 
-g <- ggplot(data=df, aes(x=mymean, xmin = ICminSG, y = myy, xend = ICmaxSG, colour=groupe)) + geom_point() 
+g <- ggplot(data=df2, aes(x=mymean, xmin = ICminSG, y = y, xend = ICmaxSG, colour=groupe)) + geom_point() 
 g <- g + labs(y = "variable")
-  
+g<-g+scale_y_continuous(name="Variables", breaks=m$y, labels=m$var)
+g
+
 for (i in 1:nrow(df)){
-g <- g + geom_segment(x = df$ICminSG[i], y = df$myy[i], xend = df$ICmaxSG[i], yend = df$myy[i], colour = col[df$colour[i]])
-g <- g + geom_segment(x = df$ICminSG[i], y = df$myy[i]-0.5, xend = df$ICminSG[i], yend = df$myy[i]+0.5, colour = col[df$colour[i]])
-g <- g + geom_segment(x = df$ICmaxSG[i], y = df$myy[i]-0.5, xend = df$ICmaxSG[i], yend = df$myy[i]+0.5, colour = col[df$colour[i]])
+  g <- g + geom_segment(x = df2$ICminSG[i], y = df2$y[i], xend = df2$ICmaxSG[i], yend = df2$y[i], colour = col[df2$colour[i]])
+  g <- g + geom_segment(x = df2$ICminSG[i], y = df2$y[i]-0.5, xend = df2$ICminSG[i], yend = df2$y[i]+0.5, colour = col[df2$colour[i]])
+  g <- g + geom_segment(x = df2$ICmaxSG[i], y = df2$y[i]-0.5, xend = df2$ICmaxSG[i], yend = df2$y[i]+0.5, colour = col[df2$colour[i]])
 }
+g
+
 
 
 #reste à faire pour le schéma : 
 #- vérifier que j'ai bien standardisé
-#- mettre en ordonné le nom de la variable
 #- enlever les trous et séparer les variables (retirer les NA puis numéroter les y en séparant les groupes de variables) : (1:20)[1:20 %% 3 !=0] 
 
-#autre manière qui permet d'afficher les noms des var et qui elimine NA
-#mais pb de limite de IC et de position de IC
-g <- ggplot(data=df, aes(x=mymean, y= var, colour=groupe)) + geom_point() 
-g <- g + labs(y = "variable")
 
-for (i in 1:nrow(df)){
-  g <- g + geom_segment(x = df$ICminSG[i], y = df$myy[i], xend = df$ICmaxSG[i], yend = df$myy[i], colour = col[df$colour[i]])
-  g <- g + geom_segment(x = df$ICminSG[i], y = df$myy[i]-0.5, xend = df$ICminSG[i], yend = df$myy[i]+0.5, colour = col[df$colour[i]])
-  g <- g + geom_segment(x = df$ICmaxSG[i], y = df$myy[i]-0.5, xend = df$ICmaxSG[i], yend = df$myy[i]+0.5, colour = col[df$colour[i]])
-}
 
 #---------
 #analysis
